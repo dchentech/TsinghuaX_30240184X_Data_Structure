bd12477de9a8ba82f1975cb5e714278572cab646
David Chen <mvjome@gmail.com>
2015-06-07 19:48:12 +0800

    Add original requirements for previous exams

--- Tsinghua_Online_Judge_problem/0.add/main.c
+++ Tsinghua_Online_Judge_problem/0.add/main.c
@@ -1,5 +1,28 @@
 #include <stdio.h>
 
+
+/*
+0.加法(Add)
+
+### 描述
+给两个正整数A和B,输出它们的和C = A + B。
+
+### 输入
+共计一行,含两个正整数 A 和 B。
+
+### 输出
+共计一行,含一个正整数,即 A 与 B 之和 C。
+
+### 输入样例
+100 200
+
+### 输出样例
+300
+
+### 限制
+1 ≤ {A, B} ≤ 106
+ */
+
 int main(int argc, const char * argv[])
 {
     double positive_integer_A, positive_integer_B;
--- Tsinghua_Online_Judge_problem/2.Divisor/main.c
+++ Tsinghua_Online_Judge_problem/2.Divisor/main.c
@@ -1,6 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+/*
+2.最大公约数(Divisor)
+
+### 描述
+任给大于 1 的两个整数 A 和 B,输出它们的最大公约数,即能够同时整除 A 和 B 的最大正整数。
+
+### 输入
+共计一行,含两个正整数 A 和 B。
+
+### 输出
+共计一行,即 A 和 B 的最大公约数。
+
+### 输入样例1
+100 35
+
+### 输出样例1
+5
+
+### 输入样例2
+121 37
+
+### 输出样例2
+1
+
+### 限制
+1 ≤ {A, B} ≤ 109
+
+*/
+
 /* Use Euclid's algorithm, see more details at https://en.wikipedia.org/wiki/Greatest_common_divisor */
 int main(int argc, const char * argv[])
 {
